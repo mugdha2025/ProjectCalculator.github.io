45b356913322c1649feb9d9812219986c1fd8116
mugdha2025 <153657567+mugdha2025@users.noreply.github.com>
2024-05-29 12:47:34 +0000

    made operators and numbers as buttons

---
+++ script.js
@@ -0,0 +1,20 @@
+const plusSign = document.getElementById('plusSign');
+const minusSign = document.getElementById('minusSign');
+const multiplicationSign = document.getElementById('multiplicationSign');
+const divisionSign = document.getElementById('divisionSign');
+
+const number1 = document.getElementById('number1');
+const number2 = document.getElementById('number2');
+const number3 = document.getElementById('number3');
+const number4 = document.getElementById('number4');
+const number5 = document.getElementById('number5');
+const number6 = document.getElementById('number6');
+const number7 = document.getElementById('number7');
+const number8 = document.getElementById('number8');
+const number9 = document.getElementById('number9');
+const number0 = document.getElementById('number0');
+
+const clearBtn = document.getElementById('clearBtn');
+
+
+
